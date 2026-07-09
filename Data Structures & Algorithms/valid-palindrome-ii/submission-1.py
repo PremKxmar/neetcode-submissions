40f9ec2d087bfc29d0020c8d7e81bfb8d0d62106
Prem Kumar <cb.sc.u4aie23052@cb.students.amrita.edu>
2026-07-09 09:58:46 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l=[]
+        left,right=0,len(s)-1
+        while left<right:
+            
+            if s[left].lower()!=s[right].lower():
+                while len(l)<2 and left<right:
+                    l.append(str(s[left]))
+                    left+=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
+            
